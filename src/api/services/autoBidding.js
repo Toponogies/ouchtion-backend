bfb87f022406bc7479917352649bafd0e0c2d32f
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-12-22 15:42:31 +0700

    add bidding module (#27)
    update database
    update product model
    add endpoint bidding
    test bidding
    add service auto-bidding

---
+++ src/api/services/autoBidding.js
@@ -0,0 +1,47 @@
+import { biddingModel, productModel } from "../models";
+
+export default async function autoBidding(){
+    try {
+        const list = await biddingModel.getAllAutoBiddingValid();
+        list.forEach(async autoBidding => {
+            const product = await productModel.getProductUseAutoBidding(autoBidding.product_id,autoBidding.bidding_id)
+            // max all autobidding
+            product.current_max_price = product.current_max_price !== null ? product.current_max_price : 0;
+            // plus 1 if step price = 0
+            product.step_price = product.step_price !== 0 ? product.step_price : 1;
+            if (product.buyer_id !== autoBidding.user_id)
+            {
+                // case if have other autobidding
+                const price_need = product.current_max_price + product.step_price;
+                if (autoBidding.max_price > product.current_max_price)
+                {
+                    // update buyer_id and current_price
+                    await productModel.patch(autoBidding.product_id,{
+                        buyer_id:autoBidding.user_id,
+                        current_price:price_need
+                    })
+                }
+                else{
+                    await biddingModel.disableOneAutoBidding(autoBidding.bidding_id);
+                }
+            }
+            else{
+                // case with normal bidding
+                const price_need = product.current_max_bid_price + product.step_price;
+                if (price_need < autoBidding.max_price && product.current_max_bid_price > product.current_price){
+                    // update current_price
+                    await productModel.patch(autoBidding.product_id,{
+                        current_price:price_need
+                    })
+                }
+                else{
+                    await biddingModel.disableOneAutoBidding(autoBidding.bidding_id);
+                }
+            }
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+    setTimeout(autoBidding, 1000);
+}
