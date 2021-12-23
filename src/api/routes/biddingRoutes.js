128fb1605f21dbab57ae5f2a0db0353202a82c7e
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-12-23 12:16:09 +0700

    change path some endpoint bidding , add endpoint get all bidding permission of one product (#31)

--- src/api/routes/biddingRoutes.js
+++ src/api/routes/biddingRoutes.js
@@ -9,11 +9,13 @@ import {schema as biddingPermissionSchema} from '../schema/biddingPermission';
 
 router.post('/',validate(biddingSchema), biddingController.addBidding) // create a bididng
 router.post('/autoBidding', validate(autobiddingSchema), biddingController.addAutoBidding) // create a auto bididng
-router.delete('/bidding/:id', biddingController.rejectBidding)
+router.delete('/rejectBidding/:id', biddingController.rejectBidding)
 
 router.get('/biddingRequest', biddingController.getBiddingRequests) // seller get all bidding request
 router.post('/biddingRequest', validate(biddingRequestPostSchema), biddingController.addBiddingRequest) // bidder send bidding request to seller
-router.put('/biddingRequest', validate(biddingPermissionSchema), biddingController.permissionBidding) // allow bidding or deny bidding
 router.delete('/biddingRequest', validate(biddingRequestSchema), biddingController.notAllowBidding) // not allow bidding permission
+
+router.post('/biddingPermission', biddingController.getBiddingPermissionProduct) // get all bidding permission of one product
+router.put('/biddingPermission', validate(biddingPermissionSchema), biddingController.permissionBidding) // allow bidding or deny bidding
 
 export default router;
