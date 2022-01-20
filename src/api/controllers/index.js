9aae901a09f1812b790b22bbc7e7556ef3c242de
Phung Bui <alex.bui.vhp@gmail.com>
2022-01-20 12:08:26 +0700

    Hot fix

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,3 +1,4 @@
 export { default as AuthController } from './authController';
 export { default as CategoryController } from './categoryController';
 export { default as ProductController } from './productController';
+export { default as UserController } from './userController';
