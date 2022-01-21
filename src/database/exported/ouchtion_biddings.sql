191046844747a58ee1b91fcfe434b106d1d57955
Phung Bui <alex.bui.vhp@gmail.com>
2022-01-22 01:29:49 +0700

    edit database schema

--- src/database/exported/ouchtion_biddings.sql
+++ src/database/exported/ouchtion_biddings.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `ouchtion` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `ouchtion`;
 -- MariaDB dump 10.19  Distrib 10.6.5-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: ouchtion
@@ -60,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-21  6:37:33
+-- Dump completed on 2022-01-22  1:29:04
