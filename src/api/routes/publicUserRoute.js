834fe21f1632a2f36e41e123264897250528b4f8
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-01-21 09:30:25 +0700

    fix update user (#56)
    Fix update user, update email, update password

--- src/api/routes/publicUserRoute.js
+++ src/api/routes/publicUserRoute.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 import { UserController } from '../controllers';
-import validate from '../middlewares/validateQuery.js';
+import validate from '../middlewares/validate';
 import { VerifySchema } from '../schemas';
 
 router.get('/:id/point', UserController.getPoint);
-router.put('/email', validate(VerifySchema), UserController.updateEmail); // update email
+router.put('/user/email', validate(VerifySchema), UserController.updateEmail); // update email
 
 export default router;
