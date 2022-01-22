8e9a28c28a96f17d9fada0caf57038fff54514a8
Ha the hien <hathehien12a2@gmail.com>
2022-01-22 21:56:25 +0700

    change price database, remove two old file database

--- src/database/exported/ouchtion_watchlists.sql
+++ src/database/exported/ouchtion_watchlists.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ouchtion` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ouchtion`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ouchtion
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-22 15:35:51
+-- Dump completed on 2022-01-22 21:54:40
