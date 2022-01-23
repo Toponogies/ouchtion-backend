440984c26aa94e7f90081e963751e48bcb930918
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2022-01-23 15:34:26 +0700

    fix request bidding, watchlist (#71)

--- src/api/routes/biddingRoutes.js
+++ src/api/routes/biddingRoutes.js
@@ -23,13 +23,13 @@ router.post('/autoBidding', isBidder, isPermittedToBid, validate(AutoBiddingSche
 router.delete('/autoBidding',validate(AutoBiddingDisableSchema) , BiddingController.disableAutoBidding);
 
 router.post('/buyProduct', isBidder, validate(BuyProductSchema), BiddingController.buyNowProduct);
-router.post('/bidders/biddingRequest', isBidder, validate(BiddingRequestPostSchema), BiddingController.addBiddingRequest);
-router.get('/bidders/biddingRequest', isBidder, validate(BiddingRequestPostSchema), BiddingController.getBiddingRequest);
-router.get('/bidders/biddingPermission', isBidder, validate(BiddingRequestPostSchema), BiddingController.getPermission);
+router.post('/bidders/biddingRequests', isBidder, validate(BiddingRequestPostSchema), BiddingController.addBiddingRequest);
+router.get('/bidders/biddingRequests/products/:id', isBidder, BiddingController.getBiddingRequest);
+router.get('/bidders/biddingPermission/products/:id', isBidder, BiddingController.getPermission);
 
 // Per seller
 router.post('/rejectBidding/:id', isSeller, BiddingController.rejectBidding);
-router.get('/sellers/biddingRequests', isSeller, validate(BiddingRequestPostSchema), BiddingController.getBiddingRequests);
+router.get('/sellers/biddingRequests/products/:id', isSeller, BiddingController.getBiddingRequests);
 router.put('/sellers/biddingRequests/:id', isSeller, validate(BiddingRequestPutSchema), BiddingController.updateRequest);
 
 router.post('/sellers/biddingPermission', isSeller, BiddingController.getBiddingPermissionProduct);
